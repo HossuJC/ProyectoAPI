841c00307f96ee15c7b2f736aa923a5e3303a2a4
Josue Cabezas <josue.a.cabezas@gmail.com>
2025-01-15 14:54:13 -0500

    Add user router and controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,77 @@
+import { Op } from "sequelize";
+import User from "../models/user.model.js";
+
+export const getUsers = async () => {
+    let data = await User.findAll();
+    return data;
+}
+
+export const getUserById = async (id) => {
+    let data = await User.findByPk(id);
+    return data;
+}
+
+export const getUsersBySearch = async (filters) => {
+    let whereFilters = {}
+
+    if (filters.name) {
+        Object.assign(whereFilters, {
+            name: {
+                [Op.iLike]: `%${filters.name}%`
+            }
+        });
+    }
+    if (filters.lastname) {
+        Object.assign(whereFilters, {
+            lastname: {
+                [Op.iLike]: `%${filters.lastname}%`
+            }
+        });
+    }
+    if (filters.email) {
+        Object.assign(whereFilters, {
+            email: {
+                [Op.iLike]: `%${filters.email}%`
+            }
+        });
+    }
+    if (filters.birthdate) {
+        Object.assign(whereFilters, {
+            birthdate: {
+                [Op.eq]: `%${filters.birthdate}%`
+            }
+        });
+    }
+
+    let data = await User.findAll({
+        where: {
+            [Op.and]: whereFilters
+        }
+    });
+    return data;
+}
+
+export const createUser = async (body) => {
+    let data = await User.create(body);
+    return data;
+}
+
+export const updateUser = async (id, body) => {
+    let data = await User.update(body, {
+        where: {
+            id: id
+        }
+    });
+    // let user = await User.findByPk(id);
+    // let data = await user.update("name", body.name);
+    return data[0];
+}
+
+export const deleteUser = async (id) => {
+    let data = await User.destroy({
+        where: {
+            id: id
+        }
+    });
+    return data;
+}
